f06d5494956a1ce1b54ea140d92c0a1108ed9e91
刘鑫 <liuxin@izaodao.org>
2020-03-10 21:33:01 +0800

    Initial commit

---
+++ shop/shop-pojo/src/main/java/com/itheima/entity/Result.java
@@ -0,0 +1,44 @@
+package com.itheima.entity;
+
+import java.io.Serializable;
+
+/**
+ * 结果实体类
+ * 封装相应状态和相应信息
+ */
+public class Result implements Serializable {
+    private Boolean success;
+    private String message;
+
+    public Result() {
+    }
+
+    public Result(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+            "success=" + success +
+            ", message='" + message + '\'' +
+            '}';
+    }
+}
